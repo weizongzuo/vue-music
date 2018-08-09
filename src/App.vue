91e0372b7b10e73ff1b58af37744a401f440319b
weizongzuo <542676491@qq.com>
2018-08-10 01:26:52 +0800

    添加头部

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    hello
+    <m-header></m-header>
   </div>
 </template>
-
 <script>
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'App'
+  components: {
+    MHeader
+  }
 }
 </script>
-
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import "~common/stylus/variable"
-
-#app
-  color: $color-theme
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import 'babel-polyfill' // 使用polyfill，需要在程序入口的顶部引入它，以确保首先加载其首先加载  这样就确保在尚未支持一些es6函数、方法的浏览器使用es6语法
 import Vue from 'vue' // 从node_module查找
 import App from './App'
 import router from './router'
 import 'common/stylus/index.styl' // 引入index.styl进来作为全局使用
 Vue.config.productionTip = false
 /* eslint-disable no-new */
+// import 'fastclick' from "fastclick";
+// fastclick.attach(document.body);
 new Vue({
   el: '#app',
   router,

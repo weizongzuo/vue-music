3403d7429a30153db9ff42f63265d6f4082990eb
weizongzuo <542676491@qq.com>
2018-11-28 22:22:14 +0800

    添加上拉滚动

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,133 +1,150 @@
 <template>
-    <div class="recommend">
-        <div class="recommend-content">
-            <!-- 轮播图  v-if="recommends.length"确保已经拿到数据再渲染slider组件 -->
-            <div v-if="recommends.length" class="slider-wrapper">
-                <slider>
-                    <div v-for="item in recommends" :key="item.id">
-                        <a :href="item.linkUrl">
-                            <img :src="item.picUrl" alt="">
-                        </a>
-                    </div>
-                </slider>
+  <div class="recommend">
+    <scroll class="recommend-content" :data='discList'>
+      <!-- 轮播图  v-if="recommends.length"确保已经拿到数据再渲染slider组件 -->
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider>
+            <div v-for="item in recommends" :key="item.id">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl" alt>
+              </a>
             </div>
-            <div class="recommend-list">
-                <h1 class="list-title">热门歌单推荐</h1>
-                <ul></ul>
-            </div>
+          </slider>
         </div>
-    </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item" :key="item.imgurl">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl" alt>
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </scroll>
+  </div>
 </template>
-<script>
-import Slider from 'base/slider/slider'
-import { getRecomend, getDiscList } from 'api/recommend'
-import { ERR_OK } from 'api/config'
-export default {
-  data() {
-    return {
-      recommends: []
-    }
-  },
-  created() {
-    this._getRecommend()
-     // 执行该方法让获取后端接口代理得到的数据
-    this._getDiscList()
-  },
-  methods: {
-    _getRecommend() {
-      // 这里是封装的promise
-      getRecomend().then(res => {
-        if (res.code === ERR_OK) {
-          this.recommends = res.data.slider
-        } else {
-          // console.log(res)
-        }
-      })
-    },
-    _getDiscList() {
-      // 这里是封装的promise
-      getDiscList().then(res => {
-        if (res.code === ERR_OK) {
-            //拿到后端接口代理请求的数据
-          console.log(res.data.list)
-        } else {
-          // 报错
-        }
-      })
-    }
-  },
-  components: {
-    Slider
-  }
-}
-</script>
-<style scoped lang="stylus" rel="stylesheet/stylus">
-@import '~common/stylus/variable';
 
-.recommend {
+<script>
+  import Slider from "base/slider/slider";
+  import { getRecomend, getDiscList } from "api/recommend";
+  import { ERR_OK } from "api/config";
+  import Scroll from "base/scroll/scroll";
+  export default {
+    data() {
+      return {
+        recommends: [],
+        discList: []
+      };
+    },
+    created() {
+      this._getRecommend()
+      // 执行该方法让获取后端接口代理得到的数据
+      this._getDiscList()
+    },
+    methods: {
+      _getRecommend() {
+        // 这里是封装的promise
+        getRecomend().then(res => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          } else {
+            // console.log(res)
+          }
+        });
+      },
+      _getDiscList() {
+        // 这里是封装的promise
+        getDiscList().then(res => {
+          if (res.code === ERR_OK) {
+            // 拿到后端接口代理请求的数据
+            this.discList = res.data.list
+          } else {
+            // 报错
+          }
+        });
+      }
+    },
+    components: {
+      Slider,
+      Scroll
+    }
+  };
+</script>
+
+<style lang="stylus" scoped>
+  @import '~common/stylus/variable';
+
+  .recommend {
     position: fixed;
     width: 100%;
     top: 88px;
     bottom: 0;
 
     .recommend-content {
-        height: 100%;
+      height: 100%;
+      overflow: hidden;
+
+      .slider-wrapper {
+        position: relative;
+        width: 100%;
         overflow: hidden;
+      }
 
-        .slider-wrapper {
-            position: relative;
-            width: 100%;
+      .recommend-list {
+        .list-title {
+          height: 65px;
+          line-height: 65px;
+          text-align: center;
+          font-size: $font-size-medium;
+          color: $color-theme;
+        }
+
+        .item {
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 20px 20px 20px;
+
+          .icon {
+            flex: 0 0 60px;
+            width: 60px;
+            padding-right: 20px;
+          }
+
+          .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 20px;
             overflow: hidden;
-        }
+            font-size: $font-size-medium;
 
-        .recommend-list {
-            .list-title {
-                height: 65px;
-                line-height: 65px;
-                text-align: center;
-                font-size: $font-size-medium;
-                color: $color-theme;
+            .name {
+              margin-bottom: 10px;
+              color: $color-text;
             }
 
-            .item {
-                display: flex;
-                box-sizing: border-box;
-                align-items: center;
-                padding: 0 20px 20px 20px;
-
-                .icon {
-                    flex: 0 0 60px;
-                    width: 60px;
-                    padding-right: 20px;
-                }
-
-                .text {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    flex: 1;
-                    line-height: 20px;
-                    overflow: hidden;
-                    font-size: $font-size-medium;
-
-                    .name {
-                        margin-bottom: 10px;
-                        color: $color-text;
-                    }
-
-                    .desc {
-                        color: $color-text-d;
-                    }
-                }
+            .desc {
+              color: $color-text-d;
             }
+          }
         }
+      }
 
-        .loading-container {
-            position: absolute;
-            width: 100%;
-            top: 50%;
-            transform: translateY(-50%);
-        }
+      .loading-container {
+        position: absolute;
+        width: 100%;
+        top: 50%;
+        transform: translateY(-50%);
+      }
     }
-}
+  }
 </style>

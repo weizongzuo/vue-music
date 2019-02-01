a976f1a5122378d3660b290c835c783a0a9f081a
weizongzuo <542676491@qq.com>
2019-02-01 21:28:26 +0800

    获取歌曲列表详情信息、以及处理为想要的数据

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,38 +1,71 @@
 <template>
-    <transition name="slide">
+  <transition name="slide">
     <div class="singer-detail"></div>
-    </transition>
+  </transition>
 </template>
 <script>
-import { mapGetters } from 'vuex';
-export default {
-    computed:{
-          // 使用对象展开运算符将 getter 混入 computed 对象中
-          // 映射为 `this.$store.getters.singer
-        ...mapGetters([
-            'singer' // 对应getters里的singer
-        ])
+  import { mapGetters } from "vuex";
+  import { getSingerDetail } from "api/singer";
+  import { ERR_OK } from "api/config";
+  import { createSong } from "common/js/song";
+  export default {
+    data() {
+      return {
+        songs: []
+      };
     },
-    created(){
-        console.log(this.singer,'ff')
+    computed: {
+      // 使用对象展开运算符将 getter 混入 computed 对象中
+      // 映射为 `this.$store.getters.singer
+      ...mapGetters([
+        "singer" // 对应getters里的singer
+      ])
+    },
+    created() {
+      this._getDetail();
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push("/singer");
+          return;
+        }
+        getSingerDetail(this.singer.id).then(res => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list);
+            console.log(this.songs)
+          }
+        });
+      },
+      _normalizeSongs(list) {
+        let ret = [];
+        list.forEach(item => {
+          let { musicData } = item;
+          if(musicData.songid && musicData.albummid){
+              ret.push(createSong(musicData))
+          }
+        });
+        return ret;
+      }
     }
-}
+  };
 </script>
 <style scoped lang='stylus' rel='stylesheet/stylus'>
-    import '~common/stylus/variable'
+  import '~common/stylus/variable', .singer-detail {
+    position: fixed;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: $color-background;
+  }
 
-    .singer-detail
-        position: fixed
-        z-index: 100
-        top:0
-        left:0
-        right:0
-        bottom:0
-        background:$color-background
+  .slide-enter-active, .slide-leave-active {
+    transition: all 0.3s;
+  }
 
-    .slide-enter-active, .slide-leave-active
-        transition: all 0.3s
-
-    .slide-enter, .slide-leave-to
-        transform: translate3d(100%, 0, 0)
+  .slide-enter, .slide-leave-to {
+    transform: translate3d(100%, 0, 0);
+  }
 </style>

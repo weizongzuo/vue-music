0e484975ecc2faf6be6cc9a4411aa8aad0149fd9
weizongzuo <542676491@qq.com>
2019-03-05 00:56:55 +0800

    完善歌手详情歌曲数据开发

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs='songs' :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 <script>
@@ -8,13 +8,23 @@
   import { getSingerDetail } from "api/singer";
   import { ERR_OK } from "api/config";
   import { createSong } from "common/js/song";
+  import MusicList from 'components/music-list/music-list'
   export default {
     data() {
       return {
         songs: []
       };
     },
+    components:{
+      MusicList
+    },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       // 使用对象展开运算符将 getter 混入 computed 对象中
       // 映射为 `this.$store.getters.singer
       ...mapGetters([
@@ -52,15 +62,7 @@
   };
 </script>
 <style scoped lang='stylus' rel='stylesheet/stylus'>
-  import '~common/stylus/variable', .singer-detail {
-    position: fixed;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: $color-background;
-  }
+  import '~common/stylus/variable'
 
   .slide-enter-active, .slide-leave-active {
     transition: all 0.3s;

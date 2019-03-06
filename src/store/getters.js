c973503148005070d701188cbdac4be060282450
weizongzuo <542676491@qq.com>
2019-03-06 22:54:09 +0800

    播放器所需store开发

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,17 @@
 export const singer = state => state.singer // 获取state.js中的是singer
+
+export const playing = state => state.playing // 获取state.js中的是playing
+
+export const fullScreen = state => state.fullScreen
+
+export const playlist = state => state.playlist
+
+export const sequenceList = state => state.sequenceList
+
+export const mode = state => state.mode
+
+export const currentIndex = state => state.currentIndex
+
+export const currentSong = (state) => { // 通过playlist播放列表以及currentIndex当前播放索引计算当前播放歌曲
+  return state.playlist[state.currentIndex] || {}
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,11 @@
+import {playMode} from 'common/js/config'
 const state = {
-  singer: {}
+  singer: {}, // 歌手
+  playing: false, // 播放状态
+  fullScreen: false, // 是否全屏
+  playlist: [], // 播放列表
+  sequenceList: [], // 顺序列表
+  mode: playMode.sequence,
+  currentIndex: -1 // 当前播放的索引
 }
 export default state
